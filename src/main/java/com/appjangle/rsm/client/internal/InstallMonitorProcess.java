56f530bca8e11c052f4139a677572b3633508909
mxro <mxro@nowhere.com>
2013-01-16 17:15:29 +1300

    exploring shutdown of monitor in failure case

--- src/main/java/com/appjangle/rsm/client/internal/InstallMonitorProcess.java
+++ src/main/java/com/appjangle/rsm/client/internal/InstallMonitorProcess.java
@@ -17,132 +17,134 @@ import com.appjangle.rsm.client.RsmClient;
 
 public class InstallMonitorProcess {
 
-	public static interface MonitorInstalledCallback {
-		public void onChangeDetected(MonitorContext ctx,
-				AtomicBoolean responseReceived);
+    public static interface MonitorInstalledCallback {
+        public void onChangeDetected(MonitorContext ctx,
+                AtomicBoolean responseReceived);
 
-		public void onFailure(Throwable t);
-	}
+        public void onFailure(Throwable t);
+    }
 
-	public final void installMonitor(final Link responsesLink,
-			final Node response, final MonitorInstalledCallback callback) {
+    public final void installMonitor(final Link responsesLink,
+            final Node response, final MonitorInstalledCallback callback) {
 
-		final AtomicBoolean responseReceived = new AtomicBoolean(false);
+        final AtomicBoolean responseReceived = new AtomicBoolean(false);
 
-		// monitor node for response from server
-		final Result<Monitor> monitor = response.monitor(Interval.FAST,
-				new Closure<MonitorContext>() {
+        // monitor node for response from server
+        final Result<Monitor> monitor = response.monitor(Interval.FAST,
+                new Closure<MonitorContext>() {
 
-					@Override
-					public void apply(final MonitorContext ctx) {
+                    @Override
+                    public void apply(final MonitorContext ctx) {
 
-						callback.onChangeDetected(ctx, responseReceived);
+                        callback.onChangeDetected(ctx, responseReceived);
 
-					}
+                    }
 
-				});
+                });
 
-		monitor.catchExceptions(new ExceptionListener() {
+        monitor.catchExceptions(new ExceptionListener() {
 
-			@Override
-			public void onFailure(final ExceptionResult r) {
-				callback.onFailure(r.exception());
-			}
-		});
+            @Override
+            public void onFailure(final ExceptionResult r) {
+                callback.onFailure(r.exception());
+            }
+        });
 
-		// trigger once after startup
-		monitor.get(new Closure<Monitor>() {
+        // trigger once after startup
+        monitor.get(new Closure<Monitor>() {
 
-			@Override
-			public void apply(final Monitor o) {
-				callback.onChangeDetected(new MonitorContext() {
+            @Override
+            public void apply(final Monitor o) {
+                callback.onChangeDetected(new MonitorContext() {
 
-					@Override
-					public Node node() {
-						return response;
-					}
+                    @Override
+                    public Node node() {
+                        return response;
+                    }
 
-					@Override
-					public Monitor monitor() {
-						return o;
-					}
-				}, responseReceived);
-			}
-		});
+                    @Override
+                    public Monitor monitor() {
+                        return o;
+                    }
+                }, responseReceived);
+            }
+        });
 
-		// to assure that monitor does not wait infinitely
-		checkForTimeout(responsesLink, response, callback, responseReceived,
-				monitor);
+        // to assure that monitor does not wait infinitely
+        checkForTimeout(responsesLink, response, callback, responseReceived,
+                monitor);
 
-	}
+    }
 
-	private void checkForTimeout(final Link responsesLink, final Node response,
-			final MonitorInstalledCallback callback,
-			final AtomicBoolean responseReceived, final Result<Monitor> monitor) {
-		new Thread() {
+    private void checkForTimeout(final Link responsesLink, final Node response,
+            final MonitorInstalledCallback callback,
+            final AtomicBoolean responseReceived, final Result<Monitor> monitor) {
+        new Thread() {
 
-			@Override
-			public void run() {
+            @Override
+            public void run() {
 
-				try {
-					int roundsLeft = RsmClient.defaultTimeoutInS;
+                try {
+                    int roundsLeft = RsmClient.defaultTimeoutInS;
 
-					while (!responseReceived.get() && roundsLeft > 0) {
-						// System.out.println("awaiting ..");
-						Thread.sleep(1000);
-						roundsLeft--;
-					}
+                    while (!responseReceived.get() && roundsLeft > 0) {
+                        // System.out.println("awaiting ..");
+                        Thread.sleep(1000);
+                        roundsLeft--;
+                    }
 
-					if (responseReceived.get()) {
-						return;
-					}
+                    if (responseReceived.get()) {
+                        return;
+                    }
 
-					// System.out.println("error");
+                    // System.out.println("error");
 
-					final ExceptionListener el = new ExceptionListener() {
+                    final ExceptionListener el = new ExceptionListener() {
 
-						@Override
-						public void onFailure(final ExceptionResult r) {
-							callback.onFailure(r.exception());
-						}
-					};
+                        @Override
+                        public void onFailure(final ExceptionResult r) {
+                            callback.onFailure(r.exception());
+                        }
+                    };
 
-					assert monitor.get() != null;
-					final Result<Success> stop = monitor.get().stop();
-					stop.catchExceptions(el);
+                    assert monitor.get() != null;
+                    final Result<Success> stop = monitor.get().stop();
+                    stop.catchExceptions(el);
 
-					// System.out.println("stopping monitor");
-					stop.get(new Closure<Success>() {
+                    // System.out.println("stopping monitor");
+                    stop.get(new Closure<Success>() {
 
-						@Override
-						public void apply(final Success o) {
-							// System.out.println("monitor stopped");
-							final Result<Success> removeSafe = responsesLink
-									.removeSafe(response);
+                        @Override
+                        public void apply(final Success o) {
+                            // System.out.println("monitor stopped");
+                            final Result<Success> removeSafe = responsesLink
+                                    .removeSafe(response);
 
-							removeSafe.catchExceptions(el);
+                            removeSafe.catchExceptions(el);
 
-							removeSafe.get(new Closure<Success>() {
+                            removeSafe.get(new Closure<Success>() {
 
-								@Override
-								public void apply(final Success o) {
+                                @Override
+                                public void apply(final Success o) {
 
-									callback.onFailure(new Exception(
-											"No response from server received in timeout (5 min)."));
+                                    callback.onFailure(new Exception(
+                                            "No response from server received in timeout (5 min)."));
 
-								}
-							});
+                                }
+                            });
 
-						}
-					});
+                        }
+                    });
 
-				} catch (final Exception e) {
-					callback.onFailure(e);
-				}
+                    // response.getSession().commit().get();
 
-			}
+                } catch (final Exception e) {
+                    callback.onFailure(e);
+                }
 
-		}.start();
-	}
+            }
+
+        }.start();
+    }
 
 }
